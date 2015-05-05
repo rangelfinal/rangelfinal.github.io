a7d71156ccb8b1e17f5e7334569b96e19ed6548a
Vitor Terçariol Rangel <rangel.final@gmail.com>
2015-05-05 16:35:52 -0300

    Grunt e Bower semi-configurados

---
+++ js/app.js
@@ -0,0 +1,4 @@
+require('./bower_components/angular/angular');
+require('./bower_components/angular-bootstrap/ui-bootstrap')
+module.exports = angular;
+angular.module('personal', ['ui.bootstrap']);
