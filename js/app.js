2e2095637674ea64b93bcb36bffb46fedb820003
Vitor Terçariol Rangel <rangel.final@gmail.com>
2015-05-05 19:27:21 -0300

    sidebar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
-require('./bower_components/angular/angular');
-require('./bower_components/angular-bootstrap/ui-bootstrap')
-module.exports = angular;
-angular.module('personal', ['ui.bootstrap']);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+var jumbontronHeight = $('.jumbotron').outerHeight(true) + 10;
+
+/* activate sidebar */
+$('.sidebar').affix({
+  offset: {
+    top: navHeight+jumbontronHeight
+  }
+});
+
+/* activate scrollspy menu */
+var $body = $(document.body);
+
+
+$body.scrollspy({
+	target: '#sidebarDiv',
+	offset: navHeight
+});
+
+/* smooth scrolling sections */
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000);
+        return false;
+      }
+    }
+});
+
+addAnchors();
