971316c78744119e2ab3daffbd301ca26c4d2b44
Vitor Terçariol Rangel <rangel.final@gmail.com>
2016-04-18 20:58:46 -0300

    Easter eggs!

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('physisApp', ['ngAnimate']).controller('physisCtrl', function($scope, $http) {
-  $scope.order = 'alphabetical';
+  $scope.order = 'boards';
   $http.get('./script/boards.json').then(r => $scope.boards = r.data.boards);
   $http.get('./script/members.json').then(r => $scope.members = r.data.members);
 });
@@ -13,14 +13,51 @@ $('.overlay').visibility({
     type: 'fixed'
 });
 
-// lazy load images
-$('.image').visibility({
-    type: 'image',
-    transition: 'vertical flip in',
-    duration: 500
-});
-
 // show dropdown on hover
 $('.ui.dropdown').dropdown({
     on: 'hover'
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(function() {
+  $('.tiny').hover(function(){
+      $(this).addClass('infinite animated tada');
+      console.log("oi");
+    },
+    function(){
+      $(this).removeClass('infinite animated tada');
+    }
+  );
+
+  $('.big.physis.logo').hover(function(){
+      $(this).addClass('infinite animated pulse');
+    },
+    function(){
+      $(this).removeClass('infinite animated pulse');
+    }
+  );
+
+  $('.menu .physis.logo').hover(function(){
+      $(this).addClass('infinite animated flip');
+    },
+    function(){
+      $(this).removeClass('infinite animated flip');
+    }
+  );
+});
+
+console.log("")
