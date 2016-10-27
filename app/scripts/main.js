6726ceee4fa04dee79c65a93e54e0401719e165c
Vitor Rangel <rangel.final@gmail.com>
2016-10-27 14:05:34 -0200

    Create new dev branch

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  defineLogoSize();
+  $( window ).resize(defineLogoSize);
+  new Vivus('logo', 1000, file: 'link/to/my.svg'}, myCallback);
+});
+
+function defineLogoSize() {
+  let width = $('.main.menu').width();
+  let height = $('.main.menu').height();
+  let size = width < height ? width : height;
+  size = (size/Math.sqrt(2))*0.75;
+  $('.logo').width(size);
+  $('.logo').height(size);
+
+  let marginLeft = (width - size)/2;
+  let marginTop = (height - size)/2;
+  let minimumMargin = (size*Math.sqrt(2) - size)/2;
+  $('.logo').css('margin-left', marginLeft > minimumMargin ? marginLeft : minimumMargin);
+  $('.logo').css('margin-top', marginTop > minimumMargin ? marginTop : minimumMargin);
+}
